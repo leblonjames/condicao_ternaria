833298c99b3cf4fe25a11be96bf60276a5f29b98
William Rocha <william.rocha@globalservs.com>
2019-08-15 16:59:09 -0300

    Inclusão de arquivo JSON e html.html

---
+++ js/main.js
@@ -0,0 +1,20 @@
+"use strict"
+
+var array = [
+
+  {
+    id: 2.322,
+
+    name: 'Bruno',
+
+    idade: 21,
+
+    email: 'bruno@email.com',
+  }
+
+];
+
+array.map((id) => console.log(id));     
+       
+       
+
